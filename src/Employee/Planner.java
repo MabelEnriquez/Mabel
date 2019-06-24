6e084ba7e58008451a5982564df8aa2469497248
Mabel <gemaenri@espol.edu.ec>
2019-06-24 10:50:48 -0500

    Agregar Menu para seleccionar Avion

--- src/Employee/Planner.java
+++ src/Employee/Planner.java
@@ -1,5 +1,8 @@
 package Employee;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Planner extends Employee {
     String codVuelo, fechaEmbarque, horaEmbarque, fechaSalida, horaSalida, fechaArribo, horaArribo, codIataSalida, codIataArribo;
     int puertaEsalida, puertaEarribo;
@@ -9,6 +12,49 @@ public class Planner extends Employee {
         super(cedula, nombre, apellido, email, usuario, clave, rol, aeroline, department);
     }
     public Planner(){
+
+    }
+    public class planificarVuelo {
+        public void main(String[] args){
+            Scanner eleccion = new Scanner(System.in);
+            boolean salir = false;
+            int opcion; // Aqui se guarda la opcion elegida por el Planner.
+            while (!salir){
+                System.out.println("1. Avion 1");
+                System.out.println("2. Avion 2");
+                System.out.println("3. Avion 3");
+                System.out.println("4. Salir");
+                try {
+                    System.out.println("Selecciona uno de los Aviones");
+                    opcion = eleccion.nextInt();
+                    switch (opcion) {
+                        case 1:
+                            System.out.println("Has seleccionado el Avion 1");
+                            break;
+                        case 2:
+                            System.out.println("Has seleccionado el Avion 2");
+                            break;
+                        case 3:
+                            System.out.println("Has seleccionado el Avion 3");
+                            break;
+                        case 4:
+                            salir = true;
+                            break;
+                        default:
+                            System.out.println("Solo números entre 1 y 4");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Debes insertar un número");
+                    eleccion.next();
+                }
+             }
+        }
+
+    }
+    public void disponibilidadVuelos(){
+
+    }
+    private void registrarAviones(){
 
     }
 }
