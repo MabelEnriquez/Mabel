fbcc037b3ccce62d426736ffcbb6672f1b056c21
Mabel <gemaenri@espol.edu.ec>
2019-06-24 00:55:14 -0500

    Agregar Atributos

--- src/Employee/Planner.java
+++ src/Employee/Planner.java
@@ -1,12 +1,11 @@
 package Employee;
 
 public class Planner extends Employee {
+    String codVuelo, fechaEmbarque, horaEmbarque, fechaSalida, horaSalida, fechaArribo, horaArribo, codIataSalida, codIataArribo;
+    int puertaEsalida, puertaEarribo;
 
     public Planner(String cedula, String nombre, String apellido, String email, String usuario, String clave, String rol, String aeroline, String department) {
         super(cedula, nombre, apellido, email, usuario, clave, rol, aeroline, department);
     }
 
-    public Planner(){
-
-    }
 }
